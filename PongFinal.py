7f3c286c4bc0ed69765376854eeaa2e224c97b0c
Prithvi Vishak <prithvivishak@gmail.com>
2021-10-02 18:01:00 +0530

    Reformat PongFinal.py

--- PongFinal.py
+++ PongFinal.py
@@ -1,86 +1,76 @@
+#!/usr/bin/python3
+
 import math
 import pygame
 import random 
 import time
 from pygame.locals import * 
 
-def main():
-    pygame.init()
+pygame.init()
+   
+pygame.display.set_caption("Pong")
+
+WIDTH = 400
+HEIGHT = 400
+BLACK = (0, 0, 0)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+FONT = pygame.font.SysFont(None, 48)
     
-    pygame.display.set_caption("Pong")
+background = pygame.image.load("assets/bg.png")
+ball = pygame.image.load("assets/Ball.png")
+paddle = pygame.image.load("assets/Paddle.png")
 
-    WIDTH = 400
-    HEIGHT = 400
-    BLACK = (0, 0, 0)
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    FONT = pygame.font.SysFont(None, 48)
+angle = 20
+xStep = 3
+yStep = 3
+x = 100
+y = 100
+
+paddleStep = 10
+X = 2
+Y = 10
+
+p = 0
+
+points = FONT.render(str(p), True, BLACK)
+
+textx = WIDTH - 30
+texty = 10
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                Y -= paddleStep
+
+            if event.key == pygame.K_DOWN:
+                Y += paddleStep
+                        
+    x += math.sin(math.radians(angle)) * xStep
+    y += math.cos(math.radians(angle)) * yStep
+
+    ballrect = ball.get_rect(topleft = (x + 23, y))
+    paddlerect = paddle.get_rect(topleft = (X + 23, Y))
+
+    if x >= HEIGHT or x <= 0:
+        xStep = -xStep
+    if y >= WIDTH or y <= 0:
+        yStep =  -yStep
+
+    if ballrect.colliderect(paddlerect):
+        p += 1
+        xStep = -xStep
     
-    background = pygame.image.load("bg.png")
-    ball = pygame.image.load("Ball.png")
-    paddle = pygame.image.load("Paddle.png")
-
-
-    angle = 20
-    xStep = 3
-    yStep = 3
-    x = 100
-    y = 100
-
-    paddleStep = 10
-    X = 2
-    Y = 10
-
-
-    p = 0
-
     points = FONT.render(str(p), True, BLACK)
 
-    textx = WIDTH - 30
-    texty = 10
-    
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    Y -= paddleStep
-
-                if event.key == pygame.K_DOWN:
-                    Y += paddleStep
-                        
-
-        
-
-        x += math.sin(math.radians(angle)) * xStep
-        y += math.cos(math.radians(angle)) * yStep
-
-        ballrect = ball.get_rect(topleft = (x + 23, y))
-        paddlerect = paddle.get_rect(topleft = (X + 23, Y))
-
-        if x >= HEIGHT or x <= 0:
-            xStep = -xStep
-        if y >= WIDTH or y <= 0:
-            yStep =  -yStep
-
-        if ballrect.colliderect(paddlerect):
-            p += 1
-            xStep = -xStep
-	
-
-        
-        points = FONT.render(str(p), True, BLACK)
-
-        screen.blit(background, (0, 0))
-        screen.blit(points, (textx, texty))
-        screen.blit(ball, (x, y))
-        screen.blit(paddle, (X, Y))
-        pygame.display.update()
-        time.sleep(0.001)
-
-            
-if __name__ == "__main__":
-    main()
+    screen.blit(background, (0, 0))
+    screen.blit(points, (textx, texty))
+    screen.blit(ball, (x, y))
+    screen.blit(paddle, (X, Y))
+    pygame.display.update()
+    time.sleep(0.001)
